0d1fc2c4a0feac2e4c1230452ed75b9cbf00e4a0
mmejiar <mario_mejram@hotmail.com>
2015-12-21 19:32:25 -0600

    21/12/2015
    Cambios en repository

--- app/static/AngularJS/Filtro/filtroRepository.js
+++ app/static/AngularJS/Filtro/filtroRepository.js
@@ -1,21 +1,21 @@
-﻿var parametroUrl = global_settings.urlCORS + '/api/filtroapi/';
+﻿var filtroUrl = global_settings.urlCORS + '/api/filtroapi/';
 
 registrationModule.factory('filtroRepository', function ($http) {
     return {
         getEmpresas: function(idEmpleado){
-            return $http.get(parametroUrl + '1|' + idEmpleado);
+            return $http.get(filtroUrl + '1|' + idEmpleado);
          },
         getSucursales: function(idEmpleado, idEmpresa){
-            return $http.get(parametroUrl + '2|' + idEmpleado + '|' + idEmpresa);
+            return $http.get(filtroUrl + '2|' + idEmpleado + '|' + idEmpresa);
         },
         getDepartamentos: function(idEmpleado, idEmpresa, idSucursal){
-            return $http.get(parametroUrl + '3|' + idEmpleado + '|' + idEmpresa + '|' + idSucursal);
+            return $http.get(filtroUrl + '3|' + idEmpleado + '|' + idEmpresa + '|' + idSucursal);
         },
         getTipoOrden: function(){
-            return $http.get(parametroUrl + '4|');
+            return $http.get(filtroUrl + '4|');
         },
         getUsuarios: function(){
-            return $http.get(parametroUrl + '5|');
+            return $http.get(filtroUrl + '5|');
         }
     };
 });
